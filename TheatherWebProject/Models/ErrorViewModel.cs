96b065b4f81d6a0857f5f5cb03643f429dae004b
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-09-10 12:46:49 +0300

    Add project files.

---
+++ TheatherWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheatherWebProject.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
