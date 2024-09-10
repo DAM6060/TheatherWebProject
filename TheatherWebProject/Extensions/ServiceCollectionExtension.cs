1e245ee56c995ccb8d25eac8ca3ff536137b68cb
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-09-10 13:13:02 +0300

    ServiceCollectionExtensions plus Program file adjustment

---
+++ TheatherWebProject/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TheatherWebProject.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtension
+	{
+		public static IServiceCollection AddApplicationServices (this IServiceCollection services)
+		{
+			return services;
+		}
+
+		public static IServiceCollection AddApplicationDbContext (this IServiceCollection services, IConfiguration config)
+		{
+			var connectionString = config.GetConnectionString("DefaultConnection");
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
+
+			//add Repository here
+
+			services.AddDatabaseDeveloperPageExceptionFilter();
+
+			return services;
+		}
+
+		public static IServiceCollection AddApplicationIdentity (this IServiceCollection services, ConfigurationManager configuration)
+		{
+			services.AddDefaultIdentity<IdentityUser>(options =>
+			{
+				options.SignIn.RequireConfirmedAccount = true;
+				options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequiredLength = 8;
+				options.User.RequireUniqueEmail = true;
+			})
+			.AddRoles<IdentityRole>()
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			return services;
+		}
+	}
+}
--- TheatherWebProject/Program.cs
+++ TheatherWebProject/Program.cs
@@ -1,17 +1,15 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using TheatherWebProject.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddApplicationServices();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,6 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseMigrationsEndPoint();
 }
 else
@@ -40,4 +39,4 @@ app.MapControllerRoute(
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-app.Run();
+await app.RunAsync();
