a672af753f7f3db8956123dfd06dd501d56cb9b4
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-09-10 17:26:45 +0300

    DBSets and Fluent API Many-Many Relationship config(Users/Plays)

--- TheatherWebProject.Infrastructure/Data/ApplicationDbContext.cs
+++ TheatherWebProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using TheatherWebProject.Infrastructure.Data.Models;
 
 namespace TheatherWebProject.Data
 {
@@ -9,5 +11,43 @@ namespace TheatherWebProject.Data
 			: base(options)
 		{
 		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			// FavouritePlays many-to-many relationship
+			builder.Entity<User>()
+				.HasMany(u => u.FavouritePlays)
+				.WithMany(p => p.UsersFavouritePlays)
+				.UsingEntity(j => j.ToTable("UserFavouritePlays"));
+
+			// LikedPlays many-to-many relationship
+			builder.Entity<User>()
+				.HasMany(u => u.LikedPlays)
+				.WithMany(p => p.UsersLikedPlays)
+				.UsingEntity(j => j.ToTable("UserLikedPlays"));
+
+			// GoingToPlays many-to-many relationship
+			builder.Entity<User>()
+				.HasMany(u => u.GoingToPlays)
+				.WithMany(p => p.UsersGoingToPlays)
+				.UsingEntity(j => j.ToTable("UserGoingToPlays"));
+
+			// WatchedPlays many-to-many relationship
+			builder.Entity<User>()
+				.HasMany(u => u.WatchedPlays)
+				.WithMany(p => p.UsersWatchedPlays)
+				.UsingEntity(j => j.ToTable("UserWatchedPlays"));
+
+			base.OnModelCreating(builder);
+
+		}
+
+		public DbSet<Play> Plays { get; set; } = null!;
+
+		public DbSet<Actor> Actors { get; set; } = null!;
+
+		public DbSet<Comment> Comments { get; set; } = null!;
+
+		public DbSet<User> Users { get; set; } = null!;
 	}
 }
