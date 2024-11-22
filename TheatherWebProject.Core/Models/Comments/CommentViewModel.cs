67349cd44518ee662781ea78a382883fbbd1197e
DAM6060 <ddimitar98@gmail.com>
2024-11-22 11:50:32 +0200

    Comment View model

--- TheatherWebProject.Core/Models/Comments/CommentViewModel.cs
+++ TheatherWebProject.Core/Models/Comments/CommentViewModel.cs
@@ -8,5 +8,17 @@ namespace TheatherWebProject.Core.Models.Comments
 {
 	public class CommentViewModel
 	{
+		public int Id { get; set; }
+		public string Content { get; set; } = string.Empty;
+
+		public DateTime CreatedOn { get; set; }
+
+		public int PlayId { get; set; }
+
+		public string PlayName { get; set; } = string.Empty;
+
+		public int ApplicationUserId { get; set; }
+
+		public string ApplicationUserDisplayName { get; set; } = string.Empty;
 	}
 }
