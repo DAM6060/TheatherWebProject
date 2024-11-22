af47d31be215b4adaa3bee9fed911d34eaac67d6
DAM6060 <ddimitar98@gmail.com>
2024-11-22 11:54:13 +0200

    Actor Form Models adjusted

--- TheatherWebProject.Core/Models/Actors/ActorFormModel.cs
+++ TheatherWebProject.Core/Models/Actors/ActorFormModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheatherWebProject.Core.Models.Plays;
 
 namespace TheatherWebProject.Core.Models.Actors
 {
@@ -15,5 +16,9 @@ namespace TheatherWebProject.Core.Models.Actors
         public string Description { get; set; } = string.Empty;
 
         public List<string> ActorImageUrls { get; set; } = new List<string>();
+
+        public List<PlayServiceModel> AllPlaysDropDown { get; set; } = new List<PlayServiceModel>();
+
+        public List<int> PlaysIds { get; set; } = new List<int>();
     }
 }
